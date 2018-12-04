6b0c78a282d2756298ff580b6883ceac1c0d1ca1
Kanta Demizu <demizu.1203@gmail.com>
2018-12-05 00:33:26 +0900

    rebuilding site 2018年 12月 5日 水曜日 00時33分26秒 JST

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo -t tale # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-cp -p -f -R public/* docs
+\cp -p -f -R public/* docs
 git add .
 # Commit changes.
 msg="rebuilding site `date`"
