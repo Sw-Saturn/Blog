b021c07c14722271be99d0f60c978773e15a061d
Kanta Demizu <demizu.1203@gmail.com>
2018-12-05 00:21:56 +0900

    rebuilding site 2018年 12月 5日 水曜日 00時21分56秒 JST

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,8 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo -t tale # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-# Go To Public folder
-cd public
-# Add changes to git.
+cp -p -f -R public/* docs
 git add .
-
 # Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
@@ -20,5 +17,3 @@ git commit -m "$msg"
 # Push source and build repos.
 git push origin master
 
-# Come Back up to the Project Root
-cd ..
